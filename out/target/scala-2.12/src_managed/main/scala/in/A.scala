4ee04e9d398f198cddd6453ac6c14514343a8bd2
DmytroMitin <dmitin3@gmail.com>
2019-07-23 16:34:04 +0300

    fix @ main

--- out/target/scala-2.12/src_managed/main/scala/in/A.scala
+++ out/target/scala-2.12/src_managed/main/scala/in/A.scala
@@ -1,5 +1,11 @@
+package in
+
+import scala.annotation.StaticAnnotation
+
 object A {
   def main(args: Array[String]): Unit = {
     println("hi")
   }
 }
+
+class main extends StaticAnnotation
--- rules/src/main/scala/Scalafixdemo.scala
+++ rules/src/main/scala/Scalafixdemo.scala
@@ -11,14 +11,15 @@ class Scalafixdemo extends SemanticRule("Scalafixdemo") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
-      case q"..$mods object $ename extends { ..$stats } with ..$inits { $self => ..$stats1 }" if mods.exists(isMain) =>
+      case tree @ q"..$mods object $ename extends { ..$stats } with ..$inits { $self => ..$stats1 }" if mods.exists(isMain) =>
         val tree1 = q"""
             ..${mods.filterNot(isMain)} object $ename extends { ..$stats } with ..$inits { $self =>
               def main(args: Array[String]): Unit = {
                 ..$stats1
               }
             }"""
-        Patch.replaceTree(doc.tree, tree1.toString)
+        Patch.replaceTree(tree, tree1.toString)
+      case _ => Patch.empty
     }.asPatch
   }
 }
